fee4b78355da26beb256916b9858e3bf60fe76ae
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-17 12:16:36 +0700

    Add project files.

---
+++ SK-Lib-cmath-ceil-Bagian2__CPP/Source.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+
+	cout << " Ceil is : " << ceil(32.43) << endl;
+	cout << " Ceil is : " << ceil(73.21) << endl;
+
+	_getch();
+	return 0;
+}
